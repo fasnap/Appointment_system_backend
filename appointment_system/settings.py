ad8675357584666dc62b4b5ea83701d6fd3eedc1
fasnap <fasnapkdy@gmail.com>
2025-02-19 12:12:14 +0530

    slash removed from cors

--- appointment_system/settings.py
+++ appointment_system/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000','appointment-system-backend-ns7d.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1','appointment-system-backend-ns7d.onrender.com']
 
 
 # Application definition
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
-    "https://appointment-system-frontend-delta.vercel.app/",
+    "https://appointment-system-frontend-delta.vercel.app",
 ]
 CORS_ALLOW_METHODS = [
     'DELETE',
